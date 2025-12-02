c22df8106ac453a3dcc0704d93478ff3b500bfd0
VANDANA RANASARA <vandanaranasara03102004@gmail.com>
2025-12-02 15:41:10 +0530

    Delete app/middleware/logger.py

--- app/middleware/logger.py
+++
@@ -1,5 +0,0 @@
-async def log_requests(request, call_next):
-    print("Request:", request.url)
-    response = await call_next(request)
-    print("Response:", response.status_code)
-    return response
